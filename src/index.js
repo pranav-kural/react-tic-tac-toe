fc8a60715c9a4438698f68576b29040a0ee8db67
pranav-kural <pranavkural042@gmail.com>
2022-04-30 13:27:46 -0600

    added logic to ascertain winner

--- src/index.js
+++ src/index.js
@@ -16,16 +16,30 @@ class Board extends React.Component {
     this.state = {
       squares: Array(9).fill(null),
       xIsNext: true,
+      status: 'Next player: X',
+      gameFinished: false,
     };
   }
 
   handleClick(i) {
-    if (!this.state.squares[i]) {
-      const squares = this.state.squares.slice();
+
+    let squares;
+    if (!this.state.gameFinished && !this.state.squares[i]) {
+      squares = this.state.squares.slice();
       squares[i] = this.state.xIsNext ? 'X' : 'O';
       this.setState({
         squares: squares, 
-        xIsNext: !this.state.xIsNext
+        xIsNext: !this.state.xIsNext,
+        status: 'Next player: ' + (!this.state.xIsNext ? 'X' : 'O'),
+      });
+    }
+
+    const winner = calculateWinner(squares);
+
+    if (winner) {
+      this.setState({
+        status: 'Winner: ' + winner + ' 🎉',
+        gameFinished: true,
       });
     }
   }
@@ -35,11 +49,10 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
 
     return (
       <div>
-        <div className="status">{status}</div>
+        <div className="status">{this.state.status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -74,6 +87,26 @@ class Game extends React.Component {
       </div>
     );
   }
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
 
 // ========================================
